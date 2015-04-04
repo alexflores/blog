222304ad15307c730588ad0f2a101cdd3cacd30a
alexflores <axel_f_g@hotmail.com>
2015-04-03 21:04:44 -0400

    Creacion del proyecto Blog y scaffold de Post

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '202a37559191d75299dd134428e3e770f1a64836396c55b8e286862968ecc8c600e0f5860c1862893a5d6abcae1cc7d7d39eb5b31139b2d702cf984ad3d1712b'
