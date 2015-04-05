1b00463d89fdeb8dd9aebd0ac9d5d3f91ec7db2e
alexflores <axel_f_g@hotmail.com>
2015-04-05 16:03:32 -0400

    formulario de busqueda, logica en el modelo post y modificacion en index del controlador post para busqueda

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
 	has_many :comments
+
+	def self.search(search)
+	  if search	
+        search_condition = "%" + search + "%"
+        find(:all, :conditions => ['content LIKE ? ', search_condition])
+      else
+        find(:all)
+      end
+    end
 end
